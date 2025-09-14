ed6f746a31bfc1fb1cbef06ff03c9f68dd0a9a13
Bhavya-mahyavanshi <bhavyamahyavanshi1534@gmail.com>
2025-09-14 17:25:53 -0400

    all folder update

---
+++ services/CartService.java
@@ -0,0 +1,28 @@
+package services;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CartService {
+    private Map<String, Integer> items;
+
+    public CartService() {
+        items = new HashMap<>();
+    }
+
+    public void addItem(String name, int quantity) {
+        items.put(name, items.getOrDefault(name, 0) + quantity);
+    }
+
+    public void removeItem(String name) {
+        items.remove(name);
+    }
+
+    public Map<String, Integer> getItems() {
+        return items;
+    }
+
+    public void clearCart() {
+        items.clear();
+    }
+}
